e833c802feda7a0c16423d89edbc8fdb4726ec0d
Choola <choolanuwanugc987@gmail.com>
2024-06-14 18:17:00 +0530

    5th commit choola

--- frontend/src/Components/Studentsignin.jsx
+++ frontend/src/Components/Studentsignin.jsx
@@ -1,44 +1,83 @@
 import React, { useState } from 'react';
-import {StudentSignInContainer, Title, Logo, FormContainer, Topic, InputField , SubmitButton , Regs} from '../Styles/StudentSignInStyles';
+import { StudentSignInContainer, Title, Logo, FormContainer, Topic, InputField, SubmitButton, Regs } from '../Styles/StudentSignInStyles';
 import ruh1 from '../../src/Assets/Ruhunalogo.png';
+import { useNavigate } from 'react-router-dom';
+
 
 const Studentsignin = () => {
 
- const [username, setUsername] = useState('');
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
- const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
- const handleSignIn = () => {
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
 
- }
- const handleRegister = () => {
+  console.log(formData);
 
- }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
- console.log(ruh1);
+    if (!formData.username || !formData.password) {
+      return setErrorMessage("All fields are required!");
+    }
+    try {
+
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("http://localhost:4000/api/auth/signinStudent", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+
+      if (res.ok) {
+        navigate('/student/dashboard');
+      }
+
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
 
     <StudentSignInContainer>
-      <Title>DEIE UGP Management System</Title> <br/>
+      <Title>DEIE UGP Management System</Title> <br />
       <Logo src={ruh1} alt="Logo" />
       <FormContainer>
         <Topic>Login</Topic>
-        <InputField 
-           type='text'
-           placeholder='Username'
-           value={username}
-           onChange={(e)=> setUsername(e.target.value)}
-           required
+        <InputField
+          type='text'
+          placeholder='Username'
+          id="username"
+          onChange={handleChange}
+          required
         />
-        
-         <InputField 
-           type='password'
-           placeholder='Password'
-           value={password}
-           onChange={(e)=> setPassword(e.target.value)}
-           required
+
+        <InputField
+          type='password'
+          placeholder='Password'
+          id='password'
+          onChange={handleChange}
+          required
         />
-        <SubmitButton to="/student/dashboard" type='button' onClick={handleSignIn}>Login</SubmitButton>
+        <SubmitButton type='button' onClick={handleSubmit}>Login</SubmitButton>
         <Regs to="/student/student-register" type='button'  >Register</Regs>
       </FormContainer>
     </StudentSignInContainer>
