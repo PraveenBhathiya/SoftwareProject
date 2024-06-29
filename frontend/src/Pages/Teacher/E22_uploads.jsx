5832fcbc535b67ed0a90b2c967e6bdea71ca21b2
praveen <praveenbandara421@gmail.com>
2024-06-29 16:52:38 +0530

    Upload pages by Praveen

---
+++ frontend/src/Pages/Teacher/E22_uploads.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const E22_uploads = () => {
+  return (
+    <div>E22_uploads</div>
+  )
+}
+
+export default E22_uploads;
---
+++ frontend/src/Pages/Teacher/Uploads.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import TeacherSidebar from './Sidebar';
+import '../../CSS/Marks.css';
+import prof from '../../Assets/profile.png';
+
+const Teacher_Uploads = () => {
+
+    const [menu, setMenu] = useState("Dashboard");
+
+  return (
+    <div className='teacher-marks'>
+    <TeacherSidebar />
+
+    <div className="marks-container">
+        <div className="title">Uploads</div>
+        <div className="profile-container">
+            <div className='profile' onClick={() => { setMenu("Dashboard") }}>
+                <img src={prof} alt="" />
+                <Link to='/profile' style={{ textDecoration: 'none' }}>Profile</Link>
+                {menu === "Profile" ? <hr /> : null}
+            </div>
+        </div>
+
+        <div className="courses">
+            <div className="e22">
+                <Link to='/e22-uploads'><button className="e22-btn">e22</button></Link>
+                <p>Final Year Undergraduate Project</p>
+            </div>
+            <div className="e21">
+                <Link to='/e21-uploads'><button className="e21-btn">e21</button></Link>
+                <p>Final Year Undergraduate Project</p>
+            </div>
+            <div className="e20">
+                <Link to='/e20-uploads'><button className="e20-btn">e20</button></Link>
+                <p>Final Year Undergraduate Project</p>
+            </div>
+            <div className="e19">
+                <Link to='/e19-uploads'><button className="e19-btn">e19</button></Link>
+                <p>Final Year Undergraduate Project</p>
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Teacher_Uploads;
