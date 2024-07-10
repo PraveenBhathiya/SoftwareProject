48c149c31f7a3b7b197f02dab8c4cc2f1a0c7ac8
Praveen Bhathiya <praveenbandara421@gmail.com>
2024-07-10 05:48:14 +0530

    UserManage_part1

--- frontend/src/Pages/Admin/ManageUser.jsx
+++ frontend/src/Pages/Admin/ManageUser.jsx
@@ -1,63 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import '../../CSS/AdminTools.css';
+import React from 'react'
 
 const ManageUser = () => {
-  const [users, setUsers] = useState([]);
-  const [showUsers, setShowUsers] = useState(false);
-
-  const fetchUserData = async () => {
-    try {
-      const response = await fetch('http://localhost:4000/api/admin/getAllStudents');
-      const data = await response.json();
-      setUsers(data);
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-    }
-  };
-
-  const handleButtonClick = () => {
-    if (!showUsers) {
-      fetchUserData();
-    }
-    setShowUsers(!showUsers);
-  };
-
   return (
-    <div className="manageUserContainer">
-      <h2 className="title">User Information</h2>
-      <button className="toggleButton" onClick={handleButtonClick}>
-        {showUsers ? 'Hide User List' : 'Show User List'}
-      </button>
-      {showUsers && (
-        <div className="userList">
-          {users.length > 0 ? (
-            users.map((user) => (
-              <div key={user._id} className="userInfo">
-                <div className="userDetail">
-                  <span className="label">Username:</span>
-                  <span className="value">{user.username}</span>
-                </div>
-                <div className="userDetail">
-                  <span className="label">Reg No:</span>
-                  <span className="value">{user.regNo}</span>
-                </div>
-                <div className="userDetail">
-                  <span className="label">Email:</span>
-                  <span className="value">{user.email}</span>
-                </div>
-                <div className="userDetail">
-                  <span className="label">Batch:</span>
-                  <span className="value">{user.batch}</span>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>No user data fetched yet.</p>
-          )}
-        </div>
-      )}
-    </div>
-  );
-};
+    <div>ManageUser</div>
+  )
+}
 
-export default ManageUser;
+export default ManageUser
