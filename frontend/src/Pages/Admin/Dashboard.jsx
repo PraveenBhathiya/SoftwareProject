c3574a58339be26fd7d1722cde7544f5e664be4e
Praveen Bhathiya <praveenbandara421@gmail.com>
2024-07-21 21:44:22 +0530

    Admin dashboard Completed Praveen

--- frontend/src/Pages/Admin/Dashboard.jsx
+++ frontend/src/Pages/Admin/Dashboard.jsx
@@ -1,57 +1,62 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import dep from '../../Assets/department.png';
-import prof from '../../Assets/profile.png';
-import '../../CSS/Dashboard.css';
+import dep from '../../Assets/rm-dash.png';
+import prof from '../../Assets/iprofile.png';
 import StuNoticePanel from '../Student/StuNoticePanel.jsx';
 import AdminSidebar from '../Admin/Sidebar.jsx';
+import '../../CSS/AdminDash.css';
+import adminlogo from '../../Assets/rm-dash.png';
+import logo4 from '../../Assets/imessages.png';
 
 const AdminDashBoard = () => {
     const [menu, setMenu] = useState("Dashboard");
 
+
     return (
-        <div className='dashContainer'>
-            
-                <AdminSidebar/>
-            
-            <div className='mainContent'>
-                <div className='leftPanel'>
-                    <div className="verticalLine"></div> {/* Thin vertical line */}
-                    <StuNoticePanel /> {/* Notice panel component */}
-                </div>
-                <div className='profile' onClick={() => { setMenu("Dashboard") }}>
-                    <img src={prof} alt="" />
-                    <Link style={{ textDecoration: 'none' }} to='/profile'>Profile</Link>
-                    {menu === "Profile" ? <hr /> : null}
-                </div>
-                <div className="hi">Hi, Good Morning!</div>
-                <div className="title">
-                    <h2>Welcome to DEIE UGP Management System</h2>
-                </div>
-                <div className="dep">
-                    <img src={dep} alt="" className="depart" />
-                </div>
-                <div className="p">Available Courses</div>
-                <div className="courses">
-                    <div className="e22">
-                        <button className="e22-btn">e22</button>
-                        <p>Final Year Undergraduate Project</p>
+       <div className="admin-main">
+        <AdminSidebar/>
+        <div className="profile-container">
+                    <div className='profile' onClick={() => { setMenu("Dashboard") }}>
+                        <img src={prof} alt="" />
+                        <Link className='Profi' to='/profile' style={{ textDecoration: 'none' }}>Profile</Link>
+                        {menu === "Profile" ? <hr /> : null}
                     </div>
-                    <div className="e21">
-                        <button className="e21-btn">e21</button>
-                        <p>Final Year Undergraduate Project</p>
+        </div>
+        <div className="Notification-container">
+                    <div className='Notification' onClick={() => { setMenu("Dashboard") }}>
+                        <img src={logo4} alt="" />
+                        <Link className='Noti' to='/Notices'  style={{ textDecoration: 'none' }}>Notifications</Link>
+                        {menu === "Notices" ? <hr /> : null}
                     </div>
-                    <div className="e20">
-                        <button className="e20-btn">e20</button>
-                        <p>Final Year Undergraduate Project</p>
-                    </div>
-                    <div className="e19">
-                        <button className="e19-btn">e19</button>
-                        <p>Final Year Undergraduate Project</p>
-                    </div>
+        </div>
+            <div className="topic-section">
+                <div className='admin-hi'>Hi, Good Morning!</div>
+                <div className='admin-welcome'>Welcome to DEIE UGP Management <br/>System</div>
+            </div>
+
+            <div className="admin-content">
+                <div className="admin-module">
+                    
+                        <div className='admin-title1'>View your marks</div>
+                        <div className="frame1">
+                            <button className="btn-1">e22</button>
+                            <p>Final Year Undergraduate Projects</p>
+                        </div>
+
+                        <div className='admin-title2'>Upload Your Progress</div>
+                        <div className="frame2">
+                            <button className="btn-2">e22</button>
+                            <p>Final Year Undergraduate Projects</p>
+                        </div>
+                    
+                </div>
+                <div className="admin-img">
+                    <img src={adminlogo} alt="" />
                 </div>
             </div>
-        </div>
+
+       </div>
+    
     );
 };
 
--- frontend/src/Pages/Admin/Sidebar.jsx
+++ frontend/src/Pages/Admin/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import logo from '../../Assets/Ruhunalogo.png'
-import logo1 from '../../Assets/dashboard.png';
-import logo2 from '../../Assets/marks.png';
-import logo3 from '../../Assets/uploads.png';
-import logo4 from '../../Assets/notices.png';
-import logo5 from '../../Assets/settings.png';
-import logo6 from '../../Assets/profile.png';
-import logo7 from '../../Assets/book.png';
+import logo from '../../Assets/rm-ruh.png';
+import logo1 from '../../Assets/ihome.png';
+import logo2 from '../../Assets/ibook.png';
+import logo3 from '../../Assets/iupload.png';
+import logo4 from '../../Assets/imessages.png';
+import logo5 from '../../Assets/isettings.png';
+import logo6 from '../../Assets/iuser.png';
+import logo7 from '../../Assets/imodule.png';
 import { Link } from 'react-router-dom';
 import '../../CSS/Sidebar.css';
 
@@ -22,13 +22,13 @@ export const AdminSidebar = ({children}) => {
       </div>
       <div className="panel">
         <ul className='nav-menu'>
-          <li onClick={()=>{setMenu("Dashboard")}}><img src={logo1} alt="Dashboard Icon" /><Link style={{textDecoration:'none'}} to='/admin/dashboard'>Dashboard</Link>{menu==="Dashboard"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("Marks")}}><img src={logo2} alt="Marks Icon" /><Link style={{textDecoration:'none'}} to='/Admin-Marks'>Marks</Link>{menu==="Marks"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("Uploads")}}><img src={logo3} alt="Uploads Icon" /><Link style={{textDecoration:'none'}} to='/Uploads'>Uploads</Link>{menu==="Uploads"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("Notices")}}><img src={logo4} alt="Notices Icon" /><Link style={{textDecoration:'none'}} to='/Notices'>Notices</Link>{menu==="Notices"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("Manage Users")}}><img src={logo6} alt="User Icon" /><Link style={{textDecoration:'none'}} to='/admin/manage-user'>Manage User</Link>{menu==="ManageUser"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("Manage Modules")}}><img src={logo7} alt="Module Icon" /><Link style={{textDecoration:'none'}} to='/ManageModules'>Manage Modules</Link>{menu==="ManageModules"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("Settings")}}><img src={logo5} alt="Settings Icon" /><Link style={{textDecoration:'none'}} to='/Settings'>Settings</Link>{menu==="Settings"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Dashboard")}}><img src={logo1} alt="Dashboard Icon" /><Link style={{textDecoration:'none',color:'#2563ea'}} to='/admin/dashboard'>Dashboard</Link>{menu==="Dashboard"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Marks")}}><img src={logo2} alt="Marks Icon" /><Link style={{textDecoration:'none',color:'#2563ea'}} to='/Admin-Marks'>Marks</Link>{menu==="Marks"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Uploads")}}><img src={logo3} alt="Uploads Icon" /><Link style={{textDecoration:'none',color:'#2563ea'}} to='/Uploads'>Uploads</Link>{menu==="Uploads"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Notices")}}><img src={logo4} alt="Notices Icon" /><Link style={{textDecoration:'none',color:'#2563ea'}} to='/Notices'>Notices</Link>{menu==="Notices"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Manage Users")}}><img src={logo6} alt="User Icon" /><Link style={{textDecoration:'none',color:'#2563ea'}} to='/admin/manage-user'>Manage User</Link>{menu==="ManageUser"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Manage Modules")}}><img src={logo7} alt="Module Icon" /><Link style={{textDecoration:'none',color:'#2563ea'}} to='/ManageModules'>Manage Modules</Link>{menu==="ManageModules"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Settings")}}><img src={logo5} alt="Settings Icon" /><Link style={{textDecoration:'none',color:'#2563ea'}} to='/Settings'>Settings</Link>{menu==="Settings"?<hr/>:<></>}</li>
         </ul>
       </div>
       <main>{children}</main>
