6de77e246727b8095823894f3a28b8f668e7a358
ravishahapuarachchi <ravishahapuarachchi@gmail.com>
2024-10-20 13:20:15 +0530

    update outer front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:4000',
-                secure: false, // we are using http, not https
-                //changeOrigin: true,
-            },
-        },
-    },
-    plugins: [react()],
-});
+  plugins: [react()],
+})
